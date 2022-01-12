2317647c92b94df04add172a667c9fa042d617af
Lucas May <lucasmayvieira@gmail.com>
2022-01-12 06:40:38 -0300

    creating folder models and properties

--- folders/models.py
+++ folders/models.py
@@ -3,4 +3,18 @@ from utils.models import BaseModel
 
 
 class Folder(BaseModel):
-    pass
+    user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='folders')
+    name = models.CharField(max_length=256, db_index=True, default='untitled folder')
+    color_hexa = models.CharField(max_length=6, null=True)
+    parent_folder = models.ForeignKey('self', null=True, on_delete=models.CASCADE)
+
+    class Meta:
+        db_table = 'FOLDER'
+
+    def __str__(self):
+        return f'{self.name} - {self.user}'
+
+    @property
+    def subfolders(self):
+        # TODO: Return all folders that has this folder as parent_folder
+        pass
