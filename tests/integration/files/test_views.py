63044461de357f9037ebd62f253fddbf22f623a0
Lucas May <lucasmayvieira@gmail.com>
2022-01-17 09:53:02 -0300

    testing file list views

--- tests/integration/files/test_views.py
+++ tests/integration/files/test_views.py
@@ -45,11 +45,57 @@ class TestFileViewSet:
 
         assert response.status_code == 401
 
-    def test_should_list_my_folders(self, api_client, folder):
-        pass
+    def test_should_list_my_files(self, api_client, file_factory):
+        # GIVEN
+        file_factory.create_batch(3, user=api_client.user)
 
-    def test_should_list_my_folders_empty(self, api_client):
-        pass
+        # WHEN
+        response = api_client.get(self.PATH)
+
+        # THEN
+        assert len(response.data) == 3
+
+    def test_should_list_my_files_empty(self, api_client):
+        # WHEN
+        response = api_client.get(self.PATH)
+
+        # THEN
+        assert not response.data
+
+    def test_should_list_my_files_in_a_folder(
+            self, api_client, file_factory,
+            folder):
+
+        # GIVEN
+        file_factory.create_batch(3, user=api_client.user)
+        file_factory.create_batch(2, user=api_client.user, folder=folder)
+
+        folder_id = str(folder.pk)
+
+        # WHEN
+        response = api_client.get(self.PATH + f'?folder={folder_id}')
+
+        # THEN
+        assert len(response.data) == 2
+
+    def test_should_list_my_files_in_a_folder_empty(
+            self, api_client, file_factory,
+            folder):
+
+        # GIVEN
+        file_factory.create_batch(3, user=api_client.user)
+
+        folder_id = str(folder.pk)
+
+        # WHEN
+        response = api_client.get(self.PATH + f'?folder={folder_id}')
+
+        # THEN
+        assert not response.data
 
     def test_should_not_list_unlogged(self, api_anonymous_client):
-        pass
+        # GIVEN WHEN
+        response = api_anonymous_client.get(self.PATH)
+
+        # THEN
+        assert response.status_code == 401
